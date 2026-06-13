7a2a5701ac9d252092e742735ae0049d074de2c2
fathimaa-hh <fasilamltr@gmail.com>
2026-06-13 09:36:25 +0530

    Initial Spring Boot project setup

---
+++ residex-backend/src/main/java/com/residex/ResidexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.residex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidexBackendApplication.class, args);
+	}
+
+}
---
+++ residex-backend/src/test/java/com/residex/ResidexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
